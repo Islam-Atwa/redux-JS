cf6b477de3957a007c331bd9c2421b6c0cfac30a
Islam Atwa <islamatwa429@gmail.com.com>
2025-08-25 00:11:56 +0300

    feat: create 3 concepts setup(action - reducer - store )

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+// console.log(Redux);
+
+
+// const action = {
+//     type : "Increment"
+// }
+// const action2 = {
+//     type : 'Decrement'
+// }
+// const reducer =(state =10, action)=>{
+//     switch(action.type){
+//         case 'Increment':
+//             return state + 1;
+
+//         case 'Decrement':
+//             return state - 1;
+
+//         default:
+//             return state;
+//     }
+// }
+
+// const store = Redux.createStore(reducer);
+// store.dispatch(action);   //11
+// store.dispatch(action);   //12
+// store.dispatch(action2);   //11
+// console.log(`the state is ${store.getState()}`);
