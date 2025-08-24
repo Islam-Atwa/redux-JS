8125b94bfc5b4fc503b0931a1ae2b5c18ee4aaa9
Islam Atwa <islamatwa429@gmail.com.com>
2025-08-24 20:31:08 +0300

    example

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+console.log(Redux);
+
+
+const action = {
+    type : "Increment"
+}
+const action2 = {
+    type : 'Decrement'
+}
+const reducer =(state =10, action)=>{
+    switch(action.type){
+        case 'Increment':
+            return state + 1;
+
+        case 'Decrement':
+            return state - 1;
+
+        default:
+            return state;
+    }
+}
+
+const store = Redux.createStore(reducer);
+console.log(`the state is ${store.getState()}`);
